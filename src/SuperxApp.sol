42aa0fd599ff2228d453ca0238a9f4433ea4aad5
PhantomOz <faniogor@gmail.com>
2024-08-07 00:21:14 +0100

    This commit adds enum PayFeesIn and custom errors

--- src/SuperxApp.sol
+++ src/SuperxApp.sol
@@ -1,10 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 /// @title SuperxApp Contract
 /// @author Favour Aniogor (@SuperDevFavour).
 /// @notice This contract implements cross contract interactions regarding transfering, swapping and Staking Tokens.
 /// @dev This contracts implements chainlink CCIP
 contract SuperxApp {
-    // created app contract
+    using SafeERC20 for IERC20;
+
+    //custom type to know what the user has decided to pay fees in
+    enum PayFeesIn {
+        Native,
+        LINK
+    }
+
+    // Custom errors to provide more descriptive revert messages.
+    error NotEnoughBalance(uint256 currentBalance, uint256 calculatedFees); // Used to make sure contract has enough balance to cover the fees.
+    error NothingToWithdraw(); // Used when trying to withdraw Ether but there's nothing to withdraw.
+    error FailedToWithdrawEth(address owner, address target, uint256 value); // Used when the withdrawal of Ether fails.
+    error DestinationChainNotAllowed(uint64 destinationChainSelector); // Used when the destination chain has not been allowlisted by the contract owner.
+    error SourceChainNotAllowed(uint64 sourceChainSelector); // Used when the source chain has not been allowlisted by the contract owner.
+    error SenderNotAllowed(address sender); // Used when the sender has not been allowlisted by the contract owner.
+    error InvalidReceiverAddress(); // Used when the receiver address is 0.
 }
