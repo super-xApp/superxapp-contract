8c40733b729019051039fb3b3fefd2929f747387
PhantomOz <faniogor@gmail.com>
2024-08-06 23:52:34 +0100

    this commit adds contract

---
+++ src/SuperxApp.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/// @title SuperxApp Contract
+/// @author Favour Aniogor (@SuperDevFavour).
+/// @notice This contract implements cross contract interactions regarding transfering, swapping and Staking Tokens.
+/// @dev This contracts implements chainlink CCIP
+contract SuperxApp {
+    // created app contract
+}
